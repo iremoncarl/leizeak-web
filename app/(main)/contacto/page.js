fb3bd0f31094fde4734a84a86484d37c9c325937
Iratxe <iratxe@tasiva.com>
2026-03-10 14:23:38 +0100

    Mejorada ui contacto

--- app/(main)/contacto/page.js
+++ app/(main)/contacto/page.js
@@ -1,5 +1,5 @@
 'use client'
-import { FaYoutube, FaInstagram, FaFacebookF, FaFacebook, FaFacebookMessenger, FaFacebookSquare } from "react-icons/fa";
+import { FaYoutube, FaInstagram, FaFacebookSquare, FaArrowRight } from "react-icons/fa";
 
 export default function Contacto() {
   const handleSubmit = (e) => {
@@ -16,32 +16,45 @@ export default function Contacto() {
       <div className="h-full grid grid-cols-1 md:grid-cols-2 gap-40 pb-30">
 
         {/* Info de contacto */}
-        <div className="flex flex-col justify-between font-serif">
+        <div className="flex flex-col">
           <div>
             <p>¡Ponte en contacto con nosotras!</p>
-            <p className="text-3xl">leizeaktaldea@gmail.com</p>
+            <p className="text-3xl mt-4">leizeaktaldea@gmail.com</p>
           </div>
-          <div className="self-end">
-            <p className="text-serif">O siguenos en nuestras redes sociales</p>
-            <div>
+          <div className="mt-14 h-full mb-10">
+            <p className="text-serif mb-4">O siguenos en nuestras redes sociales:</p>
+            <div className="h-full flex flex-col justify-between">           
+              
               <a href="https://www.youtube.com/@leizeaktaldea9731" target="_blank" rel="noopener noreferrer" aria-label="Enlace a YouTube">
-                <div className="flex items-center py-2">
-                  <FaYoutube className="w-10 h-10 p-2 text-white text-xl bg-white/20 rounded-4xl border border-white mr-4"/>
-                  <p>Nuestro canal de YouTube</p>
+                <div className="border border-white hover:border-[#8b5504] transition duration-500 rounded-md p-4 flex items-center hover:translate-x-2 group">
+                  <FaYoutube className="w-10 h-10 p-2 text-white text-xl bg-white/20 rounded-full border border-white group-hover:border-[#8b5504]/70 transition duration-1000 mr-4"/>
+                  <div>
+                    <p>YouTube</p>
+                    <p className="text-sm text-white group-hover:text-[#a27736] transition-colors duration-1000">Leizeak Taldea</p>
+                  </div>
+                  <FaArrowRight className="opacity-0 group-hover:opacity-100 transition duration-700 ml-auto text-[#8b5504]"/>
                 </div>
               </a>
               <a href="https://www.instagram.com/leizeak/" target="_blank" rel="noopener noreferrer" aria-label="Enlace a Instagram">
-                <div className="flex items-center py-2">
-                  <FaInstagram className="w-10 h-10 p-2 text-white text-xl bg-white/20 rounded-4xl border border-white mr-4"/>
-                  <p>Nuestra página de Instagram</p>
+                <div className="border border-white hover:border-[#8b5504] transition duration-500 rounded-md p-4 flex items-center hover:translate-x-2 group">
+                  <FaInstagram className="w-10 h-10 p-2 text-white text-xl bg-white/20 rounded-full border border-white group-hover:border-[#8b5504]/70 transition duration-1000 mr-4"/>
+                  <div>
+                    <p>Instagram</p>
+                    <p className="text-sm text-white group-hover:text-[#a27736] transition-colors duration-1000">@leizeak</p>
+                  </div>
+                  <FaArrowRight className="opacity-0 group-hover:opacity-100 transition duration-700 ml-auto text-[#8b5504]"/>
                 </div>
               </a>
               <a href="https://www.facebook.com/Leizeak/" target="_blank" rel="noopener noreferrer" aria-label="Enlace a Facebook">
-                <div className="flex items-center py-2">
-                  <FaFacebookSquare className="w-10 h-10 p-2 text-white text-xl bg-white/20 rounded-4xl border border-white mr-4"/>
-                  <p>Nuestra página de Facebook</p>
+                <div className="border border-white hover:border-[#8b5504] transition duration-500 rounded-md p-4 flex items-center hover:translate-x-2 group">
+                  <FaFacebookSquare className="w-10 h-10 p-2 text-white text-xl bg-white/20 rounded-full border border-white group-hover:border-[#8b5504]/70 transition duration-1000 mr-4"/>
+                  <div>
+                    <p>Facebook</p>
+                    <p className="text-sm text-white group-hover:text-[#a27736] transition-colors duration-1000">@Leizeak</p>
+                  </div>
+                  <FaArrowRight className="opacity-0 group-hover:opacity-100 transition duration-700 ml-auto text-[#8b5504]"/>
                 </div>
-              </a>
+              </a>          
             </div>
           </div>
         </div>
