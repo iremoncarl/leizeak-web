42d5a76fb5e45f44ee6be046b3800745c8af4814
iremoncarl <iremoncarl@educacion.navarra.es>
2026-03-23 21:25:47 +0100

    Creada funcionalidad para que un usuario pueda crear, modificar o eliminar un comentario en una noticia

---
+++ app/componentes/noticias/ModificarComentario.js
@@ -0,0 +1,53 @@
+'use client'
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { modificarComentarioNoticia, eliminarComentarioNoticia } from "@/lib/supabase/actions";
+import { FiTrash2, FiEdit } from "react-icons/fi";
+
+export default function ModificarComentario({comentarioId, comentario}) {
+  const router = useRouter();
+  
+  const [nuevoComentario, seNuevoComentario] = useState(comentario);
+  const [modifVisible, setModifVisible] = useState(false);
+
+  const modificarComentario = async () => {
+    console.log("Pulsado modificar comentario con id " + comentarioId)
+    await modificarComentarioNoticia(comentarioId, nuevoComentario);
+    router.refresh();
+    setModifVisible(false);
+  }
+  const eliminarComentario = async () => {
+    console.log("Pulsado eliminar comentario con id " + comentarioId)
+    await eliminarComentarioNoticia(comentarioId);
+    router.refresh();
+  }
+
+  return (
+    <div className="flex gap-4">
+      <button onClick={()=>setModifVisible(true)} className="hover:bg-white/30 transition rounded-full w-10 h-10 flex items-center justify-center">
+        <FiEdit className="w-5 h-5 cursor-pointer" />
+      </button>
+      <button onClick={eliminarComentario}  className="hover:bg-white/30 transition rounded-full w-10 h-10 flex items-center justify-center">
+        <FiTrash2 className="w-5 h-5 cursor-pointer" />
+      </button>
+
+
+      {modifVisible && 
+        <div className="fixed inset-0 bg-black/90 flex items-center justify-center z-50" >
+          <div className="text-white p-8 my-5 bg-black border border-white rounded-xl flex flex-col  w-[80%]">
+            <textarea type="text" value={nuevoComentario} onChange={(e) => seNuevoComentario(e.target.value)} className="w-full p-2 mb-4 min-h-30 rounded border border-white/20"/>
+              <div className="flex justify-between mt-8">
+                <button onClick={() => setModifVisible(false)} className="bg-[#ff0000] hover:bg-[#a27736] transition items-center px-4 py-2 rounded cursor-pointer align-end flex gap-2 self-end">
+                  <p>Cancelar</p>
+                </button> 
+                <button onClick={modificarComentario} className="bg-[#00ff00] hover:bg-[#a27736] transition items-center px-4 py-2 rounded cursor-pointer align-end flex gap-2 self-end">
+                  <p>Guardar</p>
+                </button> 
+              </div>
+                 
+              </div>
+        </div>
+      }
+    </div>
+  )
+}
---
+++ app/componentes/noticias/NuevoComentario.js
@@ -0,0 +1,26 @@
+'use client'
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { crearComentarioNoticia } from "@/lib/supabase/actions";
+import { FiSend } from "react-icons/fi";
+
+export default function NuevoComentario({noticiaId}) {
+  const router = useRouter();
+
+  const [comentario, setComentario] = useState("");
+
+  const crearComentario = async () => {
+    await crearComentarioNoticia(noticiaId, comentario);
+    router.refresh();
+  }
+
+  return (
+    <div className="text-white p-4 mb-5 bg-white/15 rounded-xl flex flex-col mt-5">
+      <textarea type="text" placeholder="Escribe aquí tu opinión" value={comentario} onChange={(e) => setComentario(e.target.value)} className="w-full p-2 mb-4 min-h-30 rounded border border-white/20"/>
+      <button onClick={crearComentario} className="bg-[#8b5504] hover:bg-[#a27736] transition items-center px-4 py-2 rounded cursor-pointer align-end flex gap-2 self-end">
+        <p>Enviar opinión</p>
+        <FiSend className="w-5 h-5"/>
+      </button>  
+    </div>
+  )
+}
