d77d72c44c2acfb2146d8473051848eebd6d21e8
iremoncarl <iremoncarl@educacion.navarra.es>
2026-03-24 19:26:02 +0100

    Añadido feedback en formulario de contacto

--- app/componentes/contacto/Formulario.js
+++ app/componentes/contacto/Formulario.js
@@ -1,5 +1,6 @@
 "use client";
-import { useActionState } from "react";
+import { useActionState, useEffect } from "react";
+import toast, { Toaster } from 'react-hot-toast';
 
 import { submitContactForm } from "@/lib/emails/actions";
 
@@ -7,8 +8,19 @@ import { submitContactForm } from "@/lib/emails/actions";
 export function Formulario({textosFormulario}) {
     const [currentState, formAction, isPending] = useActionState(submitContactForm, {});
 
+    useEffect(() => {
+        if (!currentState) return;
+
+        if (currentState.success) mostrarMensajeExito(currentState.message);
+        else mostrarMensajeError(currentState.message);
+
+    }, [currentState]);
+    
+    const mostrarMensajeError = (mensaje) => toast.error(mensaje);
+    const mostrarMensajeExito = (mensaje) => toast.success(mensaje);
+
     return (
-        <form className="w-full flex flex-col justify-between border border-white/30 rounded-xl p-8" action={formAction}>
+        <form className="w-full flex flex-col justify-between border border-white/30 rounded-xl p-8" action={formAction} method="post">
 
             <div className="flex flex-col xl:flex-row gap-0 xl:gap-8">
                 <input className="mb-8 pb-2 border-b border-white focus:border-[#a27736] outline-none placeholder-white/60" id="nombre" name="nombre" type="text" placeholder={textosFormulario.nombre}/>
@@ -24,12 +36,8 @@ export function Formulario({textosFormulario}) {
             <button className="bg-[#8b5504] hover:bg-[#a27736] transition cursor-pointer text-white font-bold py-2 px-4 rounded-3xl" type="submit">
                 {isPending ? textosFormulario.enviando : textosFormulario.enviar}
             </button>
-            {currentState.success && currentState.message && (
-                <p>{currentState.message}</p>
-            )}
-            {currentState.error && (
-                <p>{currentState.error}</p>
-            )}
+            
+            <Toaster position="top-center" containerStyle={{top: 100}}/>
         </form>
     );
 }
